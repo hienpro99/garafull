cdb96bf7adf103f27b615e0453afb7ed7df9d2f1
hienpro99 <damhien221299@gmail.com>
2022-11-28 20:54:51 +0700

    hiendd

---
+++ app/src/main/java/tuandxph21037/nhom3/gara/Model/TopNhanVien.java
@@ -0,0 +1,15 @@
+package tuandxph21037.nhom3.gara.Model;
+
+public class TopNhanVien {
+    public String tenNhanVien;
+    public int soLuong;
+
+
+    public TopNhanVien() {
+    }
+
+    public TopNhanVien(String tenNhanVien, int soLuong) {
+        this.tenNhanVien = tenNhanVien;
+        this.soLuong = soLuong;
+    }
+}
